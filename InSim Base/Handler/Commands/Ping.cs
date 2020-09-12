e067c92353309ed34a292de365592d43a178a3e2
MrSev7en <dueduardo01@hotmail.com>
2020-09-12 12:13:48 -0300

    Update Ping.cs

--- InSim Base/Handler/Commands/Ping.cs
+++ InSim Base/Handler/Commands/Ping.cs
@@ -10,16 +10,29 @@ namespace InSim_Base.Handler.Commands
     {
         public Ping(string[] Args, Connections Conn)
         {
+            Interface.Message.Send("^6» ^7Medindo sua taxa de latência, e a taxa de latência do servidor. Aguarde...", Conn.UCID);
+
             System.Net.NetworkInformation.Ping _Ping = new System.Net.NetworkInformation.Ping();
             PingReply Reply = _Ping.Send((new WebClient().DownloadString(new Uri("https://api.ipify.org/"))));
+            PingReply ReplyMe = null;
 
-            if (Reply.Status == IPStatus.Success)
+            if (!string.IsNullOrWhiteSpace(Conn.IP))
             {
-                Interface.Message.Send($"^6» ^7O ping atual desse servidor é ^6{Reply.RoundtripTime.ToString()}ms", Conn.UCID);
+                ReplyMe = _Ping.Send(Conn.IP);
             }
             else
             {
-                Interface.Message.Send($"^1» ^7O ping atual desse servidor está muito alto ou não foi possível identificar.", Conn.UCID);
+                ReplyMe = _Ping.Send("127.0.0.1");
+            }
+
+            if (Reply.Status == IPStatus.Success && ReplyMe.Status == IPStatus.Success)
+            {
+                Interface.Message.Send($"^6» ^7O ping atual desse servidor é ^6{Reply.RoundtripTime.ToString()}ms", Conn.UCID);
+                Interface.Message.Send($"^6» ^7O seu ping atual nesse servidor é ^6{ReplyMe.RoundtripTime.ToString()}ms", Conn.UCID);
+            }
+            else
+            {
+                Interface.Message.Send($"^1» ^7O seu ping ou o ping do servidor está muito alto, então não foi possível identificar.", Conn.UCID);
             }
         }
     }
